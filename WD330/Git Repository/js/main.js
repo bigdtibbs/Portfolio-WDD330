a944fd1e3e0b6938166c1191d458d5963efafae4
Daren Tibbitts <48360194+bigdtibbs@users.noreply.github.com>
2021-01-31 09:56:25 -0700

    Updates. Hopefully Git accepts them.

--- WD330/Git Repository/js/main.js
+++
@@ -1,6 +0,0 @@
-const links = [
-    {
-        label: "Week01 Notes",
-        url: "week01/index.html"
-    }
-]
---
+++ WD330/Week01/main.js
@@ -0,0 +1,19 @@
+const links = [
+    {
+        label: "Week01 Notes",
+        url: "week01/index.html"
+    },
+    {
+        label: "Week 02 Notes",
+        url: "week02/index.html"
+    },
+    {
+        label: "Week 03 Notes",
+        url: "week03/index.html"
+    },
+    {
+        label: "Week 04 Notes",
+        url: "week04/index.html"
+    }
+
+]
